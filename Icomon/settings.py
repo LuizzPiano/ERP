4de8f8ada6833c5977c48d53e6cdca6c8d9abd6e
Luiz Gustavo Cavalcante <101760453+LuizzPiano@users.noreply.github.com>
2023-04-19 21:48:56 -0300

    Update settings.py

--- Icomon/settings.py
+++ Icomon/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*", "erp-production-35b7.up.railway.app"]
 
 
 # Application definition
