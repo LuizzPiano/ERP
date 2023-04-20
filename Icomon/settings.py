3e9d26ad5e858fb98733d3c3abf91f09cea908a3
LuizzPiano <you@example.com>
2023-04-19 23:29:16 -0300

    allset

--- Icomon/settings.py
+++ Icomon/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-d5^0q@d1e#abq4nw5-w)iyj0qqokgh20v)__=!&fpwhr@5$^zr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -79,13 +79,41 @@ WSGI_APPLICATION = 'Icomon.wsgi.application'
     
 #     }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'icomonerp',
+#         'USER': 'admin',
+#         'PASSWORD': 'asdwasdw123',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#         'charset': 'utf8mb4',
+#         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#         }
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'icomonerp',
+        'USER': 'admin',
+        'PASSWORD': 'asdwasdw123',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        
     }
 }
-
 
 
 
